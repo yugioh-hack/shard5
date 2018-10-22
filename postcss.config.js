c3e0cb3efde9b323e48ad5b22511776cadfe3d56
yugioh-hack <yugiohhack.enl@gmail.com>
2018-10-22 23:27:35 +0900

    stylelintやアルファベット順にソートするプラグインの導入

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = ctx => ({
   plugins: {
-    "postcss-import": {},
+    "stylelint": ctx.env === 'development' ? {}: false,
+    "postcss-import": ctx.env === 'development' ? {
+      plugins: [
+        require("stylelint")({
+          //ignoreFiles: 'css/src/*.css' // 上のstylelintと重複するため無視
+        })
+      ]
+    }:{},
+    "css-declaration-sorter":{order: 'alphabetically'},
     "autoprefixer": {
       "browsers": "last 2 versions",
     }//,
